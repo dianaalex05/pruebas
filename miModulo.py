9ce46333f0de5c6dfd421d27bbb15adbaf8e3dd3
diana viñanzaca <42183483+dianaalex05@users.noreply.github.com>
2020-09-01 13:16:08 -0500

    Add files via upload

---
+++ miModulo.py
@@ -0,0 +1,21 @@
+# modulo logico 
+
+def sumar  (n1,n2):
+   suma = n1+n2
+   return suma
+
+def restar (n1,n2):
+     resta = n1-n2
+     return resta
+    
+def multiplicar (n1,n2):
+     multiplicacion = n1*n2
+     return multiplicacion 
+    
+def dividir(n1,n2):
+    if (n2!=0):
+        division = n1/n2
+        return division 
+    else:
+        return 0
+         
